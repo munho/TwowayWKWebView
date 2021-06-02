90c47ae70c329d0c2858a45fc10b3cc1a65fb536
Munho Kwak <munho@ub1st.co.kr>
2021-06-02 17:39:05 +0900

    two-way-communication-between-ios-wkwebview-and-web-page https://diamantidis.github.io/2020/02/02/two-way-communication-between-ios-wkwebview-and-web-page

--- TwowayWKWebView/ViewController.swift
+++ TwowayWKWebView/ViewController.swift
@@ -6,14 +6,84 @@
 //
 
 import UIKit
+import WebKit
 
+
+/*
+ https://diamantidis.github.io/2020/02/02/two-way-communication-between-ios-wkwebview-and-web-page
+ */
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.addSubview(webView)
+        NSLayoutConstraint.activate([
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+            webView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor)
+        ])
+
+        if let url = Bundle.main.url(forResource: "index", withExtension: "html") {
+            webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
+        }
+
+        addUserScript()
     }
 
+    private lazy var webView: WKWebView = {
+        let webView = WKWebView()
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        return webView
+    }()
 
+    // [1]
+    private func addUserScript() {
+        let contentController = self.webView.configuration.userContentController
+        contentController.add(self, name: "toggleMessageHandler")
+
+        let js = """
+            var _selector = document.querySelector('input[name=myCheckbox]');
+            _selector.addEventListener('change', function(event) {
+                var message = (_selector.checked) ? "Toggle Switch is on" : "Toggle Switch is off";
+                if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.toggleMessageHandler) {
+                    window.webkit.messageHandlers.toggleMessageHandler.postMessage({
+                        "message": message
+                    });
+                }
+            });
+        """
+
+        let script = WKUserScript(source: js, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
+        contentController.addUserScript(script)
+    }
 }
+
+extension ViewController: WKScriptMessageHandler{
+
+    // [2]
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        guard let dict = message.body as? [String : AnyObject] else {
+            return
+        }
+
+        print("[\(#function)] \(dict)")
+
+        guard let message = dict["message"] else {
+            return
+        }
+
+        let script = "document.getElementById('value').innerText = \"\(message)\""
+
+        webView.evaluateJavaScript(script) { (result, error) in
+            if let result = result {
+                print("Label is updated with message: \(result)")
+            } else if let error = error {
+                print("An error occurred: \(error)")
+            }
+        }
+    }
+}
+
 
